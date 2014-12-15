0d70ce563efb4d7e45fec69988caf950d8a2d514
tricuong95 <nguyentricuong1995@gmail.com>
2014-12-15 05:44:21 -0500

    Add routes to users.js

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,36 @@ var router = express.Router();
 var user_controller = require('../controllers/user_controller');
 
 /*
- * GET /users
+ * All route are prepended with /users/
+ */
+
+/*
+ * GET all users
  */
 router.get('/', user_controller.index);
 
 /*
- * POST /users/new
+ * POST user information to sign up
  */
 router.post('/new', user_controller.create);
+
+/*
+ * GET information of an user
+ */
+router.get('/:id', user_controller.show);
+
+/*
+ * GET information of all owned items of an user
+ */
+router.get('/:id/owned_items', user_controller.showOwnedItems);
+
+/*
+ * GET information of all trading items of an user
+ */
+router.get('/:id/trading_items', user_controller.showTradingItems);
+
+/*
+ * router.delete('/:id', user_controller.);
+ */
 
 module.exports = router;
