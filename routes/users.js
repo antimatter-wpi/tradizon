400bb1767c0beeb6954ed82005b0153d57a2bbef
Hoang Ngo <hoangngo.dev@gmail.com>
2014-12-15 03:20:29 -0500

    Implement method to get all users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var user_controller = require('../controllers/user_controller');
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
-});
+/*
+ * GET /users
+ */
+router.get('/', user_controller.index);
 
 module.exports = router;
