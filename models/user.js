90d6487431d2d4152ed77374651b5ec026f1227d
Hoang Ngo <hoangngo.dev@gmail.com>
2014-12-15 02:53:38 -0500

    Create User model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    email: DataTypes.STRING,
+    address: DataTypes.STRING,
+    phone: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+
+  return User;
+};
