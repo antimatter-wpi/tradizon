b5fc933a45acf00c1a578559d4f2f0066f92453c
Hoang Ngo <hoangngo.dev@gmail.com>
2014-12-15 06:24:29 -0500

    Implement method to render sign in form

---
+++ controllers/sessions_controller.js
@@ -0,0 +1,20 @@
+/*
+ * Renders a sign in form
+ */
+exports.new = function(req, res) {
+	res.render("sessions_form");
+}
+
+/*
+ * Creates a session and logs a user in
+ */
+exports.create = function(req, res) {
+
+}
+
+/*
+ * Destroys a session and logs a user out
+ */
+exports.destroy = function(req, res) {
+
+}
