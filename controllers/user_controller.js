0a10b1d37a969cc78c7642a592e0fb244ba45b1e
Hoang Ngo <hoangngo.dev@gmail.com>
2014-12-15 05:39:49 -0500

    Hash password before storing in database

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,5 @@
-var models = require('../models');
+var models = require('../models'),
+	cryptiles = require('cryptiles');
 
 /*
  * Renders a page that shows all users
@@ -22,7 +23,7 @@ exports.create = function(req, res) {
 		email: req.param('email'),
 		phone: req.param('phone'),
 		address: req.param('address'),
-		password_digest: req.param('password')
+		password_digest: cryptiles.randomString(req.param('password'))
 	})
 	.success(function() {
 		// Do something after create a user
