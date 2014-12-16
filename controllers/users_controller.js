3e7ce0efd6807bf9e38172054a1259af896c9c3c
Hoang Ngo <hoangngo.dev@gmail.com>
2014-12-15 21:52:27 -0500

    Reformat users controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -6,10 +6,9 @@ var models = require('../models'),
  * Renders a page that shows all users
  */
 exports.index = function(req, res) {
-	models.User
-	.findAll()
-	.success(function(users) {
-		res.render('users', {
+	models.User.findAll()
+	.then(function(users) {
+		res.render('users', { 
 			title: 'All Users',
 			users: users
 		});
@@ -20,19 +19,23 @@ exports.index = function(req, res) {
  * Creates new user
  */
 exports.create = function(req, res) {
-	shasum.update(req.param('password'));
+	// Calculates the digest of given password
+	var passwordDigest = shasum
+						.update(req.param('password'))
+						.digest('base64');
 	
+	// Creates new record for this user in the database
 	models.User.create({
-		email: req.param('email'),
-		phone: req.param('phone'),
-		address: req.param('address'),
-		password_digest: shasum.digest('base64')
+		email: 			 req.param('email'),
+		phone: 			 req.param('phone'),
+		address: 		 req.param('address'),
+		password_digest: passwordDigest
 	})
-	.success(function() {
-		// Do something after create a user
+	.then(function() {
+		// Do something after creating a user
 		res.redirect('/users');
 	})
-	.error(function() {
+	.catch(function(err) {
 		// Do something after failing to create a user
 		res.redirect('/');
 	});
@@ -42,7 +45,9 @@ exports.create = function(req, res) {
  * Shows all owned items of a user
  */
 exports.showOwnedItems = function(req, res) {
-	models.Item.findAll({ where: { OwnerId : req.params.id } })
+	models.Item.findAll({ 
+		where: { OwnerId : req.params.id } 
+	})
 	.then(function(items) {
 		res.send(items);
 	});
@@ -52,7 +57,9 @@ exports.showOwnedItems = function(req, res) {
  * Shows all trading of a user
  */
 exports.showTradingItems = function(req, res) {
-	models.Item.findAll({ where: { TradeeId : req.params.id } })
+	models.Item.findAll({ 
+		where: { TradeeId : req.params.id } 
+	})
 	.then(function(items) {
 		res.send(items);
 	});
