db46179e6b75bc891fe32fb6c8c5792ef4dfe393
Hoang Ngo <hoangngo.dev@gmail.com>
2014-12-16 03:16:09 -0500

    Partially implement the items controller

--- controllers/items_controller.js
+++ controllers/items_controller.js
@@ -4,18 +4,32 @@ var models = require('../models');
  * Render a form for adding a new item
  */
 exports.new = function(req, res) {
+	res.render('item_form');
 };
 
 /*
  * Creates new item
  */
 exports.create = function(req, res) {
+	models.Item.create({
+		photo: 		 req.param('photo'),
+		title: 		 req.param('title'),
+		description: req.param('description')
+	})
+	.then(function() {
+		// TODO do something after creating new item
+		res.end();
+	});
 };
 
 /*
  * Get information of an item
  */
 exports.show = function(req, res) {
+	model.Item.find(req.params.id)
+	.then(function(item) {
+		res.render('item', { item: item });
+	})
 };
 
 /*
@@ -34,6 +48,16 @@ exports.showByCategory = function(req, res) {
  * Delete an item
  */
 exports.destroy = function(req, res) {
+	// Finds the item that needs to be deleted
+	model.Item.find(req.params.id)
+	.then(function(item) {
+
+		// Deletes that item from the database
+		item.destroy()
+		.then(function() {
+			// TODO do something after delete the item here
+		})
+	});
 };
 
 
