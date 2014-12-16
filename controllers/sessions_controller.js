82eb632700397ef52416fe56aec5a6de347c7005
tricuong95 <nguyentricuong1995@gmail.com>
2014-12-15 22:03:47 -0500

    Add title to Sign In and Sign Up pages

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -16,7 +16,7 @@ exports.new = function(req, res) {
 			res.redirect('/');	
 		} else {
 			// Renders sign in form for new user
-			res.render('signin');
+			res.render('signin', {title: 'Sign In | Tradizon'});
 		}
 	});
 }
