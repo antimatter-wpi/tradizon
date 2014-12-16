17b71c5dbf1d8d01d2d0dff6aa6ff927331915ae
Hoang Ngo <hoangngo.dev@gmail.com>
2014-12-15 21:43:08 -0500

    Add categories controller

---
+++ controllers/categories_controller.js
@@ -0,0 +1,41 @@
+var models = require('../models');
+
+/*
+ * Returns all categories object
+ */
+exports.index = function(req, res) {
+	models.Category.findAll()
+	.then(function(categories) {
+		res.send(categories);
+	});
+}
+
+/*
+ * Creates a new category
+ */
+exports.new = function(req, res) {
+	// Lowercases category name to ensure consistency
+	var categoryName = req.param('categoryName').toLowerCase();
+
+	// Creates a category with the given name
+	models.Category.create({
+		name: categoryName
+	});
+}
+
+/*
+ * Deletes a category
+ */
+exports.destroy = function(req, res) {
+	// Finds the category need to be removed
+	models.Category.find(req.params.id)
+	.then(function(category) {
+
+		// Removes the category
+		category.destroy()
+		.then(function() {
+			// Do something after delete categories
+			res.redirect('/');
+		})
+	})
+}
